fd35158fe812957dcb019137cea6db23915d9ac1
Aaron Rohrbacher and Noah Gottlieb <aaron@>
2017-08-07 16:46:35 -0700

    add jquery, still in process..

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
-var inputString = prompt("Enter a sentence");
-console.log(inputString);
+var input = prompt("Enter a sentence");
+
+
+console.log(input);
 
 var firstLast = function(inputString) {
   var first = inputString.charAt(0).toUpperCase();
@@ -8,24 +10,39 @@ var firstLast = function(inputString) {
   return first + last;
 };
 
+var switchLetters = function(letters) {
+return letters.split("").reverse().join("");
 
-var letters = (firstLast(inputString));
-console.log(letters);
-
-  var switchLetters = function(letters) {
-  return letters.split("").reverse().join("");
-
-}
-
-console.log(switchLetters(letters));
-
-console.log(inputString + switchLetters(letters));
+};
 
 var letterCount = function(inputString) {
   var rounded= Math.floor(inputString.length/2);
   return inputString.charAt(rounded)+inputString;
-}
-var newString = letterCount(inputString);
+};
+
+var newString = letterCount(input);
+var letters = (firstLast(input));
+jQuery(document).ready(function() {
+  // jQuery ("h1").click(function() {
+  //   alert(input);
+  // });
+  jQuery ("h1").click(function() {
+    $("#unencoded").html('<h1 id="unencoded">'+switchLetters(newString)+"</h1>");
+    $("#encoded").hide();
+  });
+});
+
+
+
+
+console.log(letters);
+
+
+
+console.log(switchLetters(letters));
+
+console.log(input + switchLetters(letters));
+
 console.log(newString);
 
 console.log(switchLetters(newString));
